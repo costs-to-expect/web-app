be5c57bac5523127fd63454ebc30c5131e1f995e
Dean Blackborough <dean@g3d-development.com>
2018-09-17 00:32:23 +0100

    Brand title
    * Changed brand title.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="{{ action('IndexController@recent') }}">Costs to Expect</a>
+            <a class="navbar-brand" href="{{ action('IndexController@recent') }}">Costs-to-Expect.com</a>
             @if ($display_nav_options === true)
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
