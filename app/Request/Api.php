7c0fd448d3d86b792db1d599ead3c44652c44c85
Dean Blackborough <dean@g3d-development.com>
2018-10-12 23:18:16 +0100

    Working on API class
    * Working on API class

---
+++ app/Request/Api.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Request;
+
+/**
+ * Request helper class for connecting to the Costs to Expect API
+ *
+ * @author Dean Blackborough <dean@g3d-development.com>
+ * @copyright Dean Blackborough 2018
+ * @license https://github.com/costs-to-expect/api/blob/master/LICENSE
+ * @package App\Request
+ */
+class Api
+{
+
+}
