564e9caa7a2b3fe3d6f60c2c27948c19ef521f7c
Dean Blackborough <dean@g3d-development.com>
2018-11-19 23:16:08 +0000

    Removed values from config
    * Removed API category ids from config, now defined in env file.
    * Remove database connection details from env.example, no local database.

--- config/web/config.php
+++ config/web/config.php
@@ -4,6 +4,10 @@ $key_api_resource_name = 'API_RESOURCE_NAME';
 $key_api_resource_type_id = 'API_RESOURCE_TYPE_ID';
 $key_api_resource_id = 'API_RESOURCE_ID';
 
+$key_api_category_essentials = 'API_CATEGORY_ID_ESSENTIALS';
+$key_api_category_non_essentials = 'API_CATEGORY_ID_NON_ESSENTIALS';
+$key_api_category_hobbies = 'API_CATEGORY_ID_HOBBIES';
+
 return [
     'api_base_url' => 'https://api.costs-to-expect.com/v1/',
 
@@ -31,9 +35,9 @@ return [
     'api_resource_type_id' => env($key_api_resource_type_id, null),
     'api_resource_id' => env($key_api_resource_id, null),
 
-    'api_category_id_essentials' => '98WLap7Bx3',
-    'api_category_id_non_essentials' => 'RjXM5VJDw6',
-    'api_category_id_hobbies_and_interests' => 'Gwg7zgL316',
+    'api_category_id_essentials' => env($key_api_category_essentials, null),
+    'api_category_id_non_essentials' => env($key_api_category_non_essentials, null),
+    'api_category_id_hobbies_and_interests' => env($key_api_category_hobbies, null),
 
     'version' => 'v1.03.0',
     'release_date' => '5th November 2018'
